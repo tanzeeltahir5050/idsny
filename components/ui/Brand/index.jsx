65cc6e7ff0cf5579bc8b38c349f45f0c6afb47d1
Muhammad Hamza <hardworkerhamza@gmail.com>
2023-12-27 17:27:05 +0500

    export added

--- components/ui/Brand/index.jsx
+++ components/ui/Brand/index.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import ctaImage from "../../../public/idsny.jpg";
 
 const Brand = () => (
     <Link href="/">
         <Image
-            src="/idsny.jpg"
+            src={ctaImage}
             width={40}
             height={30}
             alt="Blinder logo"
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // output: 'export',
+  // distDir: 'out',
+    images: {
+      unoptimized: true,
+    },
+    trailingSlash: false,
 }
 
 module.exports = nextConfig
